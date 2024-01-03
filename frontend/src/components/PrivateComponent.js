3cb5bbba955b2678172950d0c391ed57c14fe1a6
kashish <arorakashish519@gmail.com>
2024-01-03 21:22:45 +0530

    day5

--- frontend/src/components/PrivateComponent.js
+++ frontend/src/components/PrivateComponent.js
@@ -4,5 +4,6 @@ const PrivateComponent = () =>{
     const auth = localStorage.getItem('user')
     // cant goto other nav till loged in
     return auth? <Outlet/>:<Navigate to='/signup'/>
+    // returs
 }
 export default PrivateComponent;
